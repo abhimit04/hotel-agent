ffecda3929fa21bb3b6e30a2466729878533f611
abhimit04 <abhimit04@gmail.com>
2025-09-13 11:51:42 +0530

    Commit

--- pages/api/hotels.js
+++ pages/api/hotels.js
@@ -89,7 +89,7 @@ export default async function handler(req, res) {
          // --- Step 3: Save to cache (memory + supabase) ---
          memoryCache.set(cacheKey, { data: sortedHotels, timestamp: now });
 
-        //Send to Gemini for rerank ---
+        //Send to Gemini for rerank --
 
         const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
             const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
