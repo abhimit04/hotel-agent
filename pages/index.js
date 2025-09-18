0ac66dac30ab6cd1ec0503ee51b857dcead2dd02
abhimit04 <abhimit04@gmail.com>
2025-09-19 00:25:22 +0530

    Commit

--- pages/index.js
+++ pages/index.js
@@ -413,7 +413,7 @@ const openHotelDetails = async (hotel) => {
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
                       </svg>
                       <p className="font-semibold">
-                        Agent Score: <span className="text-emerald-300">{h.agent_score}</span>
+                        Agent Score: <span className="text-emerald-300">{h.agent_score}{h.label}</span>
                       </p>
                     </div>
                   )}
