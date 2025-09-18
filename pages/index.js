c8440d994c2d80cd9e4990ab96347c08e3701b0d
abhimit04 <abhimit04@gmail.com>
2025-09-19 02:02:19 +0530

    Commit

--- pages/index.js
+++ pages/index.js
@@ -429,7 +429,7 @@ const openHotelDetails = async (hotel) => {
                           query: {
                                   checkin_date: checkin,
                                   checkout_date: checkout,
-                                  location: h.city_name
+                                  location: h.address
                                   }
                         })
                       }
