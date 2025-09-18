bf825510d9681e6e49ce33d83e55a350e631f771
abhimit04 <abhimit04@gmail.com>
2025-09-19 00:32:33 +0530

    Commit

--- pages/index.js
+++ pages/index.js
@@ -395,7 +395,7 @@ const openHotelDetails = async (hotel) => {
                     <svg className="w-4 h-4 text-cyan-400" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
                     </svg>
-                    <p className="text-sm">{h.address}</p>
+                    <p className="text-sm">{h.label}</p> //{h.address}
                   </div>
 
                   <div className="flex items-center gap-2">
@@ -403,7 +403,7 @@ const openHotelDetails = async (hotel) => {
                       <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
                     </svg>
                     <p className="font-semibold">
-                      Review Score: <span className="text-yellow-300">{h.review_score}{h.label}</span> ({h.review_count} reviews)
+                      Review Score: <span className="text-yellow-300">{h.review_score}</span> ({h.review_count} reviews)
                     </p>
                   </div>
 
